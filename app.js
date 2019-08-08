c8ad3e3e821403c9b67181b2ab9ecb9f5881c75a
Gupta <amrita.gupta@capgemini.com>
2019-08-08 16:16:57 +0530

    add code

--- app.js
+++ app.js
@@ -10,36 +10,73 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 /**set port using env variable for server */
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+app.listen(server_port, server_host, function () {
+  console.log('Listening on port %d', server_port);
 });
 
 /**pass incoming webhook to send messege to slack from azure */
 var MY_SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
 var slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
 
-var jenkins = require('jenkins')({ baseUrl: 'http://amrita:amrita123@10.24.217.67:8080:8080', crumbIssuer: true });
+var jenkins = require('jenkins')({ baseUrl: 'http://amrita:amrita123@localhost:8080', crumbIssuer: true });
 
 app.post('/test', (req, response) => {
   console.log("Display name ", req.body.queryResult.intent.displayName);
-  console.log("vttt")
   switch (req.body.queryResult.intent.displayName) {
     case "enablejob":
-        var jobname = (req.body.queryResult.parameters.jobname).toString();
-        console.log("here")
-        jenkins.job.enable(jobname, function(err,result) {
-          if (err) {
-            console.log("error", err)
-          }else{
-            response.send(JSON.stringify({ "fulfillmentText": "Job Enabled "}));
-          }
-         
-        });
+      var jobname = (req.body.queryResult.parameters.jobname).toString();
+      jenkins.job.enable(jobname, function (err, result) {
+        if (err) {
+          console.log("error", err)
+        } else {
+          response.send(JSON.stringify({ "fulfillmentText": "Job Enabled " }));
+        }
 
-    break;
+      });
+      break;
+    case "buildstatus":
+      var buildNumber = (req.body.queryResult.parameters.buildnumber).toString();
+      var jobname = (req.body.queryResult.parameters.jobname).toString();;
+      jenkins.build.get(jobname, buildNumber, function (err, data) {
+        if (err) throw err;
+        console.log('build', data);
+        response.send(JSON.stringify({ "fulfillmentText": "Job Enabled " }));
+      });
+      break;
+    case "enablejob":
+      var jobname = (req.body.queryResult.parameters.jobname).toString();
+      jenkins.job.enable(jobname, function (err,result) {
+        if (err) {
+          console.log("Error", err)
+        } else{
+        response.send(JSON.stringify({ "fulfillmentText": "Job Enabled " }));
+      }
+      });
+      break;
+    case "disablejob":
+      var jobname = (req.body.queryResult.parameters.jobname).toString();
+      jenkins.job.disable(jobname, function (err, result) {
+        if (err) {
+          console.log("Error", err)
+        } else {
+          response.send(JSON.stringify({ "fulfillmentText": "Job Enabled " }));
+        }
+        
+      });
+      break;
+    case "jobexist":
+      var jobname = (req.body.queryResult.parameters.jobname).toString();
+      jenkins.job.disable(jobname, function (err, result) {
+        if (err) {
+         console.log("Error", err);
+        } else { 
+          response.send(JSON.stringify({ "fulfillmentText": "Job Enabled " }));
+        };
+      });
+      break;
   }
-  
+
 })
 
