507b406c54b0bcd7a49bf70030b3ca785f8696d7
Gupta <amrita.gupta@capgemini.com>
2019-08-14 10:47:14 +0530

    add code

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(server_port, server_host, function () {
   console.log('Listening on port %d', server_port);
 });
+app.timeout = 500000;
 
 var jenkins = require('jenkins')({ baseUrl: 'http://amrita:amrita123@10.75.65.182:8080', crumbIssuer: true });
 //var jenkins = require('jenkins')({ baseUrl: 'http://amrita:amrita123@192.168.43.171:8080', crumbIssuer: false });
